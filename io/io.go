ff4cee09cf91a4264d29d464aad2c022647d86ba
Jacob Brunsting <jbrunsting@uwaterloo.ca>
2020-04-01 21:10:07 -0400

    Make searching UI work

--- io/io.go
+++ io/io.go
@@ -2,13 +2,56 @@ package io
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
+	"sort"
 
 	ui "github.com/gizak/termui/v3"
 	"github.com/gizak/termui/v3/widgets"
 )
 
+const (
+	LEN_PENALTY     = -10
+	MIN_LEN_PENALTY = -125
+	MATCHING_WEIGHT = 50
+)
+
+func getMatching(entry, searchKey string) int {
+	sIndex := 0
+	matching := 0
+	for _, c := range entry {
+		if sIndex >= len(searchKey) {
+			break
+		}
+		if c == []rune(searchKey)[sIndex] {
+			matching += 1
+			sIndex += 1
+		}
+	}
+	return matching
+}
+
+func getScore(entry, searchKey string) int {
+	lenPenalty := len(entry) * LEN_PENALTY
+	if lenPenalty < MIN_LEN_PENALTY {
+		lenPenalty = MIN_LEN_PENALTY
+	}
+
+	return MATCHING_WEIGHT*getMatching(entry, searchKey) + lenPenalty
+}
+
+func sortNames(rows []string, searchKey string) {
+	sort.Slice(rows, func(i, j int) bool {
+		return getScore(rows[i], searchKey) > getScore(rows[j], searchKey)
+	})
+}
+
 func SearchForNote(dir string) string {
+	files, err := ioutil.ReadDir(dir)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	if err := ui.Init(); err != nil {
 		log.Fatalf("failed to initialize termui: %v", err)
 	}
@@ -16,34 +59,29 @@ func SearchForNote(dir string) string {
 
 	l := widgets.NewList()
 	l.Title = "> "
-	l.Rows = []string{
-		"[0] github.com/gizak/termui/v3",
-		"[1] [你好，世界](fg:blue)",
-		"[2] [こんにちは世界](fg:red)",
-		"[3] [color](fg:white,bg:green) output",
-		"[4] output.go",
-		"[5] random_out.go",
-		"[6] dashboard.go",
-		"[7] foo",
-		"[8] bar",
-		"[9] baz",
-		"[10] bar",
-		"[11] baz",
+	l.Rows = []string{}
+	for _, f := range files {
+		n := f.Name()
+		if len(n) >= 3 && n[len(n)-3:] == ".md" {
+			l.Rows = append(l.Rows, n[:len(n)-3])
+		}
 	}
 	l.TextStyle = ui.NewStyle(ui.ColorYellow)
 	l.WrapText = false
 	l.SetRect(0, 0, 80, 13)
 	l.Border = false
-	ui.Render(l)
 
 	searchKey := ""
+	sortNames(l.Rows, searchKey)
+	ui.Render(l)
+
 	uiEvents := ui.PollEvents()
 	for {
 		e := <-uiEvents
 		switch e.ID {
-		case "<C-j>":
+		case "<C-j>", "<Down>":
 			l.ScrollDown()
-		case "<C-k>":
+		case "<C-k>", "<Up>":
 			l.ScrollUp()
 		case "<C-c>":
 			return ""
@@ -57,20 +95,22 @@ func SearchForNote(dir string) string {
 			l.ScrollPageUp()
 		case "<Enter>":
 			return "" // TODO: Return selected row
+		default:
+			if e.Type == ui.KeyboardEvent {
+				if e.ID == "<Space>" {
+					searchKey += " "
+				} else if e.ID == "<Backspace>" {
+					if len(searchKey) != 0 {
+						searchKey = searchKey[:len(searchKey)-1]
+					}
+				} else if len(e.ID) == 1 {
+					searchKey += e.ID
+				}
+				l.Title = fmt.Sprintf("> %s", searchKey)
+				sortNames(l.Rows, searchKey)
+			}
 		}
 
-		if e.Type == ui.KeyboardEvent {
-			if e.ID == "<Space>" {
-				searchKey += " "
-			} else if e.ID == "<Backspace>" {
-				if len(searchKey) != 0 {
-					searchKey = searchKey[:len(searchKey)-1]
-				}
-			} else if len(e.ID) == 1 {
-				searchKey += e.ID
-			}
-			l.Title = fmt.Sprintf("> %s", searchKey)
-		}
 		ui.Render(l)
 	}
 	return ""
